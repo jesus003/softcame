986c7d6d078f81261af573846167b80fa29a2928
Jesus Ontiveros Ruiz <jesusontiveros003@gmail.com>
2018-03-08 16:30:14 -0700

    Redise;o en actividad de bienvenida, y dise;o en la actividad login

--- Bicicletas Cano/app/src/main/java/com/softsolutions/jesusontiveros/bicicletascano/login.java
+++ Bicicletas Cano/app/src/main/java/com/softsolutions/jesusontiveros/bicicletascano/login.java
@@ -1,21 +1,14 @@
 package com.softsolutions.jesusontiveros.bicicletascano;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
-    }
-    public void goLogin(View v){
-        Intent i = new Intent(this,login.class);
-        startActivity(i);
-
     }
 }
